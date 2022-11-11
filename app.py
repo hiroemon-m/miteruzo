005ab61c7f53ca6baf3c130f742d4fddda474ab3
yoyoinoyoi <y.mar.97p25@gmail.com>
2022-11-11 16:34:49 +0900

    init commit

--- app.py
+++ app.py
@@ -1,39 +1,7 @@
-import cv2
-from flask import Flask, render_template, Response
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
 
-from camera import Camera
+st.title("My first Streamlit app")
+st.write("Hello, world")
 
-app = Flask(__name__, static_folder='./templates/images')
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/stream")
-def stream():
-    return render_template("stream.html")
-
-# カメラからフレーム取得できる限り、画像を返す関数
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        print("カメラインスタンスからフレームを取得")
-        print(frame)
-
-
-        if frame is not None:
-            yield (b"--frame\r\n"
-                b"Content-Type: image/jpeg\r\n\r\n" + frame.tobytes() + b"\r\n")
-        else:
-            print("frame is none")
-
-@app.route("/video_feed")
-def video_feed():
-    return Response(gen(Camera()),
-            mimetype="multipart/x-mixed-replace; boundary=frame")
-
-
-if __name__ == "__main__":
-    app.debug = True
-    app.run(host="0.0.0.0", port=3000)
+webrtc_streamer(key="example")
