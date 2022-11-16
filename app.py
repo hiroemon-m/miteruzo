32493538dc93d893316baa006b569d4724127b22
hiroemon-m <73891312+hiroemon-m@users.noreply.github.com>
2022-11-16 13:12:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ from streamlit_webrtc import webrtc_streamer
 st.title("My first Streamlit app")
 st.write("Hello, world")
 
-webrtc_streamer(key="example")
+webrtc_streamer(key="example",
+               rtc_configuration={
+                 "iceServers":[{"urls":["stun:stun.l.google.com:19302"]}]
+               }
+               )
